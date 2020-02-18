6eb9f3ceaf1140e874c82dee60dc7effde3f4fb8
collincornman <36053489+collincornman@users.noreply.github.com>
2020-02-17 19:39:20 -0600

    Update py_test.py

--- test_scripts/py_test.py
+++ test_scripts/py_test.py
@@ -4,22 +4,22 @@ import pytest
 def test_zip_folders():
     x = 5
     y = 6
-    assert x+1 == y, "test failed"
+    assert x + 1 == y, "test failed"
 
 
 def test_unzip_to_numpy():
     x = 5
     y = 6
-    assert x+1 == y, "test failed"
+    assert x + 1 == y, "test failed"
 
 
 def test_info_return():
     x = 5
     y = 6
-    assert x+1 == y, "test failed"
+    assert x + 1 == y, "test failed"
 
 
 def test_preprocess():
     x = 5
     y = 6
-    assert x+1 == y, "test failed"
+    assert x + 1 == y, "test failed"
